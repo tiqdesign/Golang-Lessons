7feee6b046d868d08521baee474942c5da09c252
tiqdesign <tiqqq24@gmail.com>
2019-07-09 10:03:40 +0300

    updated

--- go-functions/function.go
+++ go-functions/function.go
@@ -8,7 +8,7 @@ func main() {
 	//Burada biz bir array yolladık ama bunun dışında bu arrayın istediğimiz bir kısmını kesip yollayabilirdik.
 	number := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	variadicFunc(number...)
-
+	//----------------------------
 	example1 := add(2, 8)
 	fmt.Println(example1)
 	example2 := multiply(2, 8)
@@ -18,6 +18,19 @@ func main() {
 	//-----------------------------
 	num, name := multireturn()
 	fmt.Println(name, num)
+	//-----------------------------
+	nextInt := intSeq()
+	//nextInt i sanki bir fonksiyonmuş gibi yazıyoruz nedeni ise içerisindeki isimsiz fonksiyonu çalıştırmak.
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	//bir daha fonksiyonu baska bir isimle calıstırıp bir değere atarsak bu sefer en bastan başlar
+	newInts := intSeq()
+	fmt.Println(newInts())
+
+	//output : 120 = 5!
+	fact := recursionFunc(5)
+	fmt.Println(fact)
 }
 
 func add(val1, val2 int) int {
@@ -52,4 +65,23 @@ func variadicFunc(number ...int) {
 		sum += num
 	}
 	fmt.Println(sum)
+}
+
+//Closure - içerisinde anonymous function bulundurur( herhangi bir ismi olmayan func)
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+//Recursion Function = kendi kendini cagıran fonksiyonlardır. Faktöriyel hesabı yaparak denemiş olduk
+func recursionFunc(number int) int {
+
+	if number == 0 {
+		return 1
+	} else {
+		return number * recursionFunc(number-1)
+	}
 }
